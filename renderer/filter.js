5afaf266a6919ee6fa4164cade83bd235c3c0068
JinHengyu <2195868682@qq.com>
2018-06-03 08:34:38 +0800

    滤镜大放送

--- renderer/filter.js
+++ renderer/filter.js
@@ -1,16 +1,25 @@
+global['blur'] = 0
+global['brightness'] = 100
 
 
-function toggleFilter(){
-    if(filter.isShow){
-        aside.style.right='-30%'
-        filter.isShow=false;
-    }else{
-        aside.style.right='0'
-        filter.isShow=true;
+
+global.updateFilter = () => {
+    const filter_expression = `blur(${blur}px) brightness(${brightness}%)`
+    video.style.filter = filter_expression
+
+}
+
+function toggleFilter() {
+    if (filter.isShow) {
+        aside.style.right = '-30%'
+        filter.isShow = false;
+    } else {
+        aside.style.right = '0'
+        filter.isShow = true;
     }
 }
 
 
-filter.isShow=false;
-filter.addEventListener('click',toggleFilter)
-video.addEventListener('click',toggleFilter)
+filter.isShow = false;
+filter.addEventListener('click', toggleFilter)
+video.addEventListener('click', toggleFilter)
